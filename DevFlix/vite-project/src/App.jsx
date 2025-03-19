95a1da55745977ccfcc016f2c3c69ec5cef58069
Joaquim <jocaq124@gmail.com>
2025-03-19 10:45:26 -0300

    Adicionado TmdbApi

--- DevFlix/vite-project/src/App.jsx
+++ DevFlix/vite-project/src/App.jsx
@@ -6,7 +6,18 @@ import './App.css'
 import Header from "./components/header/Header"
 
 function App() {
- 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTkzMDUyNDNhNDI5YmMxYjA1YmI0MTRlZGRkZGEzMiIsIm5iZiI6MTc0MjM4MDc5MS4xMDMwMDAyLCJzdWIiOiI2N2RhOWVmNzU5NGNhYzFlZTc2Y2JiOTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.u7rZuT1Bi3QRnJ2FUAKaNgjCIqEZ9pqS5uD5e6ZnYTE'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/authentication', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 
   return (
     <>
