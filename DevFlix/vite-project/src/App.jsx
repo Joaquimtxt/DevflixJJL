c7062ea132dad9be7bf9d687fca9d08ee13aceaf
Luiz G. <luiz.mwlo76@gmail.com>
2025-03-19 10:35:42 -0300

    - Criação do Footer + Responsividade - Criação da aba de Recomendado (em desenvolvimento) Aplicação no App.jsx

--- DevFlix/vite-project/src/App.jsx
+++ DevFlix/vite-project/src/App.jsx
@@ -3,13 +3,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import './App.css'
+import Footer from "./components/footer/Footer"
+import Recommended from "./components/recommended/Recommended"
 
 function App() {
  
 
   return (
     <>
-      
+      <Recommended />
+    <Footer />
     </>
   )
 }
---
+++ DevFlix/vite-project/src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import styles from './Footer.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+const Footer = () => {
+  return (
+     <footer className='mt-3'>
+        <div className='row'>
+            <div className='col-12 col-md-4 text-center'>
+                <a href="https://instagram.com/netflix" className='text-decoration-none text-danger fw-bolder mx-3'><img src='https://placehold.co/60x60'></img></a>
+                <a href="https://instagram.com/netflix" className='text-decoration-none text-danger fw-bolder mx-3'><img src='https://placehold.co/60x60'></img></a>
+                <a href="https://instagram.com/netflix" className='text-decoration-none text-danger fw-bolder mx-3'><img src='https://placehold.co/60x60'></img></a>
+
+            </div>
+            <div className='col-12 col-md text-center mt-2 mt-md-0 p-md-3 text-decoration-none '>
+                <div className='credits text-danger'>
+                Projeto desenvolvido por: 
+                   <a href='https://github.com/JoaoFCosta' className='fw-bolder text-decoration-none text-secondary'>  @JoaoFCosta </a> | <a href='https://github.com/JoaoFCosta' className='text-decoration-none fw-bolder text-secondary'> @Joaquimtxt </a> | <a href='https://github.com/JoaoFCosta' className='text-secondary text-decoration-none fw-bolder'> @LLuizXL </a>
+                </div>
+            </div>
+        </div>
+        <div className='row'>
+        </div>
+     </footer>
+  )
+}
+
+export default Footer
