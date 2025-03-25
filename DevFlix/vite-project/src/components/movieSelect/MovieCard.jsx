faf44884303c32d7d289d3dd0ea77340168d7ac9
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-03-25 08:52:10 -0300

    Arrumando o texto do footer

--- DevFlix/vite-project/src/components/movieSelect/MovieCard.jsx
+++ DevFlix/vite-project/src/components/movieSelect/MovieCard.jsx
@@ -1,20 +1,20 @@
-import styles from './MovieSelect.module.css'
+import styles from "./MovieSelect.module.css";
 
 const MovieCard = (props) => {
   return (
     <div className={`m-1 card-body ${styles.card}`}>
-    <div>
-     <p>{props.Year}</p>
-     </div> 
-<div>
-     <img src={props.Poster} alt=""></img> 
-     </div>
-     <div className={`card-title text-light`}>
-     <h3 >{props.Title}</h3>  
-     <span>{props.Type}</span>
-   </div>
-   </div>
-  )
-}
+      <div>
+        <p>{props.Year}</p>
+      </div>
+      <div>
+        <img src={props.Poster} alt=""></img>
+      </div>
+      <div className={`card-title text-light`}>
+        <h3>{props.Title}</h3>
+        <span>{props.Type}</span>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
