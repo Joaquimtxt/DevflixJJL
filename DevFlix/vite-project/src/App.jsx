f4de87f8bd74615de2151b8e0504e84617305543
Luiz G. <luiz.mwlo76@gmail.com>
2025-03-25 17:09:53 -0300

    ajuste no app adição da url da api e autenticação

--- DevFlix/vite-project/src/App.jsx
+++ DevFlix/vite-project/src/App.jsx
@@ -10,6 +10,17 @@ import Movies from './Pages/MoviesPage/Movies';
 import Series from './Pages/SeriesPage/Series';
 
 const App = () => {
+
+  const apiUrl = "https://api.themoviedb.org/3/";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTkzMDUyNDNhNDI5YmMxYjA1YmI0MTRlZGRkZGEzMiIsIm5iZiI6MTc0MjM4MDc5MS4xMDMwMDAyLCJzdWIiOiI2N2RhOWVmNzU5NGNhYzFlZTc2Y2JiOTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.u7rZuT1Bi3QRnJ2FUAKaNgjCIqEZ9pqS5uD5e6ZnYTE",
+    },
+  };
+
   const mudaTema = () => {
     const tema = window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
