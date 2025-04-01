d77e7964536149cde29f77e00e0ba60c53f4c12c
Joaquim <jocaq124@gmail.com>
2025-04-01 08:36:15 -0300

    Correção

--- DevFlix/vite-project/src/Pages/Home/Home.jsx
+++ DevFlix/vite-project/src/Pages/Home/Home.jsx
@@ -6,7 +6,6 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import { useEffect, useState } from "react";
 import Modal from "../../components/Modal/Modal";
 
-
 function Home() {
   const apiUrl = "https://api.themoviedb.org/3/";
   const options = {
@@ -26,9 +25,8 @@ function Home() {
   const [kidsMovies, setKidsMovies] = useState([]);
   const [animationMovies, setAnimationMovies] = useState([]);
   const [ScifiMovies, setScifiMovies] = useState([]);
-  
+
   const [pesquisar, setPesquisar] = useState("");
-  
 
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -42,31 +40,34 @@ function Home() {
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 
+  const getKidsMovie = async () => {
+    const response = await fetch(
+      `${apiUrl}discover/tv?language=pt-br&with_genres=10762&vote_average.gte=8&with_original_language=en`,
+      options
+    );
+    const data = await response.json();
 
+    setKidsMovies(data.results[0]);
+  };
+  const getAnimationMovie = async () => {
+    const response = await fetch(
+      `${apiUrl}discover/tv?language=pt-br&with_genres=16&vote_average.gte=8&with_original_language=en`,
+      options
+    );
+    const data = await response.json();
 
-const getKidsMovie = async () =>{
-  const response = await fetch(`${apiUrl}discover/tv?language=pt-br&with_genres=10762&vote_average.gte=8&with_original_language=en`, options)
-  const data = await response.json()
+    setAnimationMovies(data.results[0]);
+  };
 
-  setKidsMovies(data.results[0])
+  const getRealityMovie = async () => {
+    const response = await fetch(
+      `${apiUrl}discover/tv?language=pt-br&with_genres=10764&vote_average.gte=8&with_original_language=en`,
+      options
+    );
+    const data = await response.json();
 
-  
-
-}
-const getAnimationMovie = async () => {
-  const response = await fetch(`${apiUrl}discover/tv?language=pt-br&with_genres=16&vote_average.gte=8&with_original_language=en`, options)
-  const data = await response.json()
-
-  setAnimationMovies(data.results[0])
-}
-
-const getRealityMovie = async () => {
-  const response = await fetch(`${apiUrl}discover/tv?language=pt-br&with_genres=10764&vote_average.gte=8&with_original_language=en`, options)
-  const data = await response.json()
-
-  setRealityMovies(data.results[0])
-}
-
+    setRealityMovies(data.results[0]);
+  };
 
   const searchTitle = async (titulo) => {
     const response = await fetch(
@@ -88,39 +89,41 @@ const getRealityMovie = async () => {
     setRecommended(data.results[2]);
   };
 
-
   const getPopular = async () => {
-     const response = await fetch(`${apiUrl}movie/popular?language=pt-br&page=1`,
-       options
-      );
-     const data = await response.json();
+    const response = await fetch(
+      `${apiUrl}movie/popular?language=pt-br&page=1`,
+      options
+    );
+    const data = await response.json();
     setPopular(data.results);
   };
 
+  const getPopularSeries = async () => {
+    const response = await fetch(
+      `${apiUrl}trending/tv/week?language=pt-br`.options
+    );
+    const data = await response.json();
 
-const getPopularSeries = async () =>{
-
-  const response = await fetch(`${apiUrl}trending/tv/week?language=pt-br`. options)
-  const data = await response.json();
-
-  setPopularSeries(data.results);
-}
+    setPopularSeries(data.results);
+  };
   useEffect(() => {
-    getAnimationMovie()
-    getKidsMovie()
-    getRealityMovie()
+    getAnimationMovie();
+    getKidsMovie();
+    getRealityMovie();
     searchTitle("");
     getRecommended(setRecommended);
     getPopular(setPopular);
-    getPopularSeries(setPopularSeries)
-
+    getPopularSeries(setPopularSeries);
   }, []);
 
   return (
     <div>
       <div className="container mt-4 mb-3 overflow-hidden">
         <div className="search-bar-container">
-          <form onSubmit={handleSearch} className="d-flex align-items-center w-100 ">
+          <form
+            onSubmit={handleSearch}
+            className="d-flex align-items-center w-100 "
+          >
             <div className="input-group mb-3">
               <input
                 className=" text-light border border-4 border-danger-subtle form-control fs-4 bg-black"
@@ -135,7 +138,6 @@ const getPopularSeries = async () =>{
           </form>
         </div>
       </div>
-
 
       <Recommended
         Title={recommendedMovie.title}
@@ -152,49 +154,31 @@ const getPopularSeries = async () =>{
         <div>
           <h1 className="m-4">Pesquisa</h1>
 
-          
           <MovieSelect movies={movies} />
         </div>
       )}
 
-      
-
-<div>
-
-
+      <div>
         <h1>Novidades Devflix</h1>
         <MovieSelect movies={popularMovies} />
 
         <h1>Séries em Alta</h1>
         <MovieSelect series={popularSeries} />
-        
-        />
       </div>
 
-
-
-
-
-<Category
-  Title={kidsMovies.name}
-  Poster={kidsMovies.poster_path}
-  Categoria={kidsMovies.vote_average}
-
-  Title2={animationMovies.name}
-  Poster2={animationMovies.poster_path}
-  Categoria2={animationMovies.vote_average}
-
-  Title3={realityMovies.name}
-  Poster3={realityMovies.poster_path}
-  Categoria3={realityMovies.vote_average}
-/>
-
-
-  </div>
-
-  )
-  }
-
-
+      <Category
+        Title={kidsMovies.name}
+        Poster={kidsMovies.poster_path}
+        Categoria={kidsMovies.vote_average}
+        Title2={animationMovies.name}
+        Poster2={animationMovies.poster_path}
+        Categoria2={animationMovies.vote_average}
+        Title3={realityMovies.name}
+        Poster3={realityMovies.poster_path}
+        Categoria3={realityMovies.vote_average}
+      />
+    </div>
+  );
+}
 
 export default Home;
